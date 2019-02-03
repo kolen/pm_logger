7fb32a04e8b5be3742fbed1786691f655f42717e
kolen <incredible.angst@gmail.com>
2019-02-04 00:24:43 +0300

    Rewrite DataRecorder to c++

--- src/pm_sensor/data_recorder.h
+++ src/pm_sensor/data_recorder.h
@@ -3,15 +3,83 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <stdexcept>
 
-struct data_recorder_t {
-  char* data_buffer;
-  size_t sample_size;
-  size_t num_samples_capacity;
-  size_t num_samples_filled;
-  int32_t sampling_period;
-  int32_t last_sample_time;
-};
+namespace pm_sensor {
+  template<typename T>
+    class DataRecorder {
+  public:
+  DataRecorder(T *data_buffer, size_t capacity, int32_t sampling_period) :
+    num_samples_capacity(capacity),
+      num_samples_filled(0),
+      sampling_period(sampling_period),
+      last_sample_time(0),
+      data_buffer(data_buffer) {
+	memset(data_buffer, 0, capacity * sizeof(T));
+      }
 
-void data_recorder_init(struct data_recorder_t *self, char* data_buffer, size_t sample_size, size_t num_samples, int32_t sampling_period);
-int data_recorder_add_sample(struct data_recorder_t *self, int32_t sample_time, const void* sample);
+    void add_sample(T& sample, int32_t sample_time);
+    T get_sample(int32_t sample_time) const;
+
+    const size_t num_samples_capacity;
+    const size_t num_samples_filled;
+    const int32_t sampling_period;
+    int32_t last_sample_time;
+    T *data_buffer; // TODO: make api to access it
+  private:
+    void shift_data(int num_samples);
+  };
+
+  template<typename T>
+  void DataRecorder<T>::shift_data(int num_samples) {
+    if (!num_samples) return;
+    // Not efficient, but less error-prone than multiple pointers or
+    // something like that
+    int num_samples_moved = num_samples_capacity - num_samples;
+
+    memcpy(data_buffer + (num_samples * sizeof(T)),
+	   data_buffer,
+	   num_samples_moved * sizeof(T));
+    memset(data_buffer, 0, num_samples * sizeof(T));
+  }
+
+  template<typename T>
+  void DataRecorder<T>::add_sample(T& sample, int32_t sample_time) {
+    int skip_samples;
+    if (last_sample_time) {
+      int32_t time_from_last = sample_time - last_sample_time;
+
+      if (time_from_last % sampling_period != 0) {
+	throw std::invalid_argument("Invalid sample time");
+      }
+
+      int32_t distance = time_from_last / sampling_period;
+      shift_data(distance);
+    } else {
+      skip_samples = 0;
+    }
+
+    std::memcpy(data_buffer, &sample, sizeof(T));
+    last_sample_time = sample_time;
+  }
+
+  template<typename T>
+  T DataRecorder<T>::get_sample(int32_t sample_time) const {
+    if (!last_sample_time) {
+      return T();
+    }
+    auto time_from_last = sample_time - last_sample_time;
+
+    if (time_from_last % sampling_period != 0) {
+      throw std::invalid_argument("Invalid sample time");
+    }
+
+    auto cell_index = time_from_last / sampling_period;
+
+    if (cell_index >= num_samples_capacity) {
+      return T();
+    } else {
+      return data_buffer[cell_index];
+    }
+  }
+}
--- test/test_data_recorder.cpp
+++ test/test_data_recorder.cpp
@@ -1,7 +1,24 @@
 #include <gtest/gtest.h>
 #include "pm_sensor/data_recorder.h"
 
+struct MyDataPoint {
+  int pressure;
+  int dow_jones_average;
+
+  bool operator==(const MyDataPoint other) const {
+    return pressure == other.pressure && dow_jones_average == other.dow_jones_average;
+  }
+};
+
 TEST(DataRecorder, Basics) {
+  MyDataPoint storage[6];
+  pm_sensor::DataRecorder<MyDataPoint> data_recorder(storage, 6, 60);
+  int32_t time_base = 23874723;
+  MyDataPoint pt = {130, 250};
+  data_recorder.add_sample(pt, time_base + 0);
+  ASSERT_EQ(pt, data_recorder.get_sample(time_base));
+  ASSERT_EQ(MyDataPoint(), data_recorder.get_sample(time_base + 60*6));
+  ASSERT_EQ(MyDataPoint(), data_recorder.get_sample(time_base + 60*100));
 }
 
 int main(int argc, char **argv) {
