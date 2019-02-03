c12e481199c766b08a3db22fb8cc1f8a12f062e2
kolen <incredible.angst@gmail.com>
2019-02-04 00:24:43 +0300

    Rewrite DataRecorder to c++

--- src/pm_sensor/data_store.h
+++ src/pm_sensor/data_store.h
@@ -1,15 +1,35 @@
 #pragma once
 #include <cstdint>
+#include "pm_sensor/data_recorder.h"
 
 namespace pm_sensor {
+  const int temp_humidity_period = 10 * 60;
+  const int temp_humidity_capacity = 24 * 10;
+
+  class TemperatureHumidityMeasurement {
+  public:
+    int16_t temperature;
+    int16_t humidity;
+  };
+
   class DataStore {
   public:
-    DataStore() {};
-    int16_t current_temperature = 0;
-    int16_t current_humidity = 0;
-    int16_t current_pm2_5 = 0;
-    int16_t current_pm10 = 0;
+  DataStore() :
+    current_temperature(0),
+      current_humidity(0),
+      current_pm2_5(0),
+      current_pm10(0),
+      temp_humidity_recorder(temp_humidity_data, temp_humidity_capacity, temp_humidity_period)
+	{ }
+
+    int16_t current_temperature;
+    int16_t current_humidity;
+    int16_t current_pm2_5;
+    int16_t current_pm10;
 
     static int16_t float_to_stored(float value);
+
+    TemperatureHumidityMeasurement temp_humidity_data[temp_humidity_capacity];
+    DataRecorder<TemperatureHumidityMeasurement> temp_humidity_recorder;
   };
 }
