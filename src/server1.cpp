b2ab64bfdf249fecf57248df862a17b3af91b142
kolen <incredible.angst@gmail.com>
2019-02-03 03:18:07 +0300

    Respond with data via udp

--- src/server1.cpp
+++ src/server1.cpp
@@ -17,14 +17,29 @@ void pm_sensor::Server::start() {
 
 void pm_sensor::Server::handle(int packet_size) {
   int len = udp.read(incoming_buffer, INCOMING_BUFFER_SIZE);
+  respond();
 }
 
 void pm_sensor::Server::respond() {
+  uint8_t buffer[8];
+
+  buffer[0] = data.current_temperature & 0xff;
+  buffer[1] = (data.current_temperature & 0xff00) >> 8;
+  buffer[2] = data.current_humidity & 0xff;
+  buffer[3] = (data.current_humidity & 0xff00) >> 8;
+  buffer[4] = data.current_pm2_5 & 0xff;
+  buffer[5] = (data.current_pm2_5 & 0xff00) >> 8;
+  buffer[6] = data.current_pm10 & 0xff;
+  buffer[7] = (data.current_pm10 & 0xff00) >> 8;
+
   udp.beginPacket(udp.remoteIP(), udp.remotePort());
-  udp.write("Test");
+  udp.write(buffer, 8);
   udp.endPacket();
 }
 
 void pm_sensor::Server::tick() {
   int packet_size = udp.parsePacket();
+  if (packet_size) {
+    handle(packet_size);
+  }
 }
