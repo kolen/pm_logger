9a4e1ee2badf5a8f5542e8fe6a8c8f11b394b6ad
kolen <incredible.angst@gmail.com>
2019-02-04 18:56:01 +0300

    Add initialization to scheduler

--- src/pm_sensor/scheduler.h
+++ src/pm_sensor/scheduler.h
@@ -6,6 +6,8 @@
 namespace pm_sensor {
   class Scheduler {
   public:
+  Scheduler(): hourly_callback(), minutely_callback(), hourly_hours_mask(0), minutely_period(0),
+      last_known_time(0), hourly_last_run(0), minutely_last_run(0) { };
     std::function<void (int32_t)> hourly_callback;
     std::function<void (int32_t)> minutely_callback;
     uint32_t hourly_hours_mask;
