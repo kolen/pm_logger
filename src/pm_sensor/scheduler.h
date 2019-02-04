89787260576d4074a3951c81d90aeb98583e08af
kolen <incredible.angst@gmail.com>
2019-02-04 03:17:56 +0300

    Convert scheduler to C++

--- src/pm_sensor/scheduler.h
+++ src/pm_sensor/scheduler.h
@@ -1,23 +1,20 @@
 #pragma once
 
 #include <cstdint>
+#include <functional>
 
-struct scheduler_t {
-  void (*hourly_callback)(void*, int32_t);
-  void (*minutely_callback)(void*, int32_t);
-  void* hourly_callback_arg;
-  void* minutely_callback_arg;
+namespace pm_sensor {
+  class Scheduler {
+  public:
+    std::function<void (int32_t)> hourly_callback;
+    std::function<void (int32_t)> minutely_callback;
+    uint32_t hourly_hours_mask;
+    int minutely_period;
 
-  int32_t last_known_time;
-
-  uint32_t hourly_hours_mask;
-  int32_t hourly_last_run;
-
-  int minutely_period;
-  int32_t minutely_last_run;
-};
-
-void scheduler_init(struct scheduler_t *self);
-void scheduler_set_hourly(struct scheduler_t *self, uint32_t hours_mask, void (*callback)(void*, int32_t), void *callback_arg);
-void scheduler_set_minutely(struct scheduler_t *self, int minutely_period, void (*callback)(void*, int32_t), void *callback_arg);
-void scheduler_tick(struct scheduler_t *self, int32_t current_time);
+    void tick(int32_t current_time);
+  private:
+    int32_t last_known_time;
+    int32_t hourly_last_run;
+    int32_t minutely_last_run;
+  };
+}
