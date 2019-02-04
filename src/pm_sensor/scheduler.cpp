d725f9e345d354cd23ab2c1093bbac03cb0e6287
kolen <incredible.angst@gmail.com>
2019-02-04 19:33:05 +0300

    Remove prefixes from schedulers' fields

--- src/pm_sensor/scheduler.cpp
+++ src/pm_sensor/scheduler.cpp
@@ -15,18 +15,6 @@ static int scheduler_should_run_in_hour(int hour_number, uint32_t hours_mask) {
   return hours_mask & (1 << (23 - hour_number));
 }
 
-static int32_t scheduler_beginning_of_minute(int32_t time) {
-  return time - (time % 60);
-}
-
-static int scheduler_minute_number(int32_t time) {
-  return (time % SECONDS_IN_HOUR) / 60;
-}
-
-static int scheduler_should_run_in_minute(int minute_number, int minutely_period) {
-  return minutely_period && ((minute_number % minutely_period) == 0);
-}
-
 void pm_sensor::HourlyScheduler::tick(int32_t current_time) {
   if (!(last_known_time < current_time)) {
     return; // Detected "backwards clock", might happen when correcting it
@@ -36,11 +24,23 @@ void pm_sensor::HourlyScheduler::tick(int32_t current_time) {
   int32_t current_hour = scheduler_beginning_of_hour(current_time);
   int current_hour_number = scheduler_hour_number(current_hour);
 
-  if (current_hour > hourly_last_run &&
-      scheduler_should_run_in_hour(current_hour_number, hourly_hours_mask)) {
-    hourly_last_run = current_hour;
+  if (current_hour > last_run &&
+      scheduler_should_run_in_hour(current_hour_number, hours_mask)) {
+    last_run = current_hour;
     callback(current_hour);
   }
+}
+
+static int32_t scheduler_beginning_of_minute(int32_t time) {
+  return time - (time % 60);
+}
+
+static int scheduler_minute_number(int32_t time) {
+  return (time % SECONDS_IN_HOUR) / 60;
+}
+
+static int scheduler_should_run_in_minute(int minute_number, int period) {
+  return period && ((minute_number % period) == 0);
 }
 
 void pm_sensor::MinutelyScheduler::tick(int32_t current_time) {
@@ -52,9 +52,9 @@ void pm_sensor::MinutelyScheduler::tick(int32_t current_time) {
   int32_t current_minute = scheduler_beginning_of_minute(current_time);
   int current_minute_number = scheduler_minute_number(current_minute);
 
-  if (current_minute > minutely_last_run &&
-      scheduler_should_run_in_minute(current_minute_number, minutely_period)) {
-    minutely_last_run = current_minute;
+  if (current_minute > last_run &&
+      scheduler_should_run_in_minute(current_minute_number, period)) {
+    last_run = current_minute;
     callback(current_minute);
   }
 }
--- src/pm_sensor/scheduler.h
+++ src/pm_sensor/scheduler.h
@@ -7,22 +7,22 @@ namespace pm_sensor {
   class HourlyScheduler {
   public:
     std::function<void (int32_t)> callback {};
-    uint32_t hourly_hours_mask = 0;
+    uint32_t hours_mask = 0;
 
     void tick(int32_t current_time);
   private:
     int32_t last_known_time = 0;
-    int32_t hourly_last_run = 0;
+    int32_t last_run = 0;
   };
 
   class MinutelyScheduler {
   public:
     std::function<void (int32_t)> callback {};
-    int minutely_period = 0;
+    int period = 0;
 
     void tick(int32_t current_time);
   private:
     int32_t last_known_time = 0;
-    int32_t minutely_last_run = 0;
+    int32_t last_run = 0;
   };
 }
