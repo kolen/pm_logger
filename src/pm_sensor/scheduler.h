d949acc30e2bb3c0c602d3a0958518a32e3e2f4a
kolen <incredible.angst@gmail.com>
2019-02-04 19:22:35 +0300

    Separate schedulers to hourly and minutely

--- src/pm_sensor/scheduler.h
+++ src/pm_sensor/scheduler.h
@@ -4,17 +4,25 @@
 #include <functional>
 
 namespace pm_sensor {
-  class Scheduler {
+  class HourlyScheduler {
   public:
-    std::function<void (int32_t)> hourly_callback {};
-    std::function<void (int32_t)> minutely_callback {};
+    std::function<void (int32_t)> callback {};
     uint32_t hourly_hours_mask = 0;
-    int minutely_period = 0;
 
     void tick(int32_t current_time);
   private:
     int32_t last_known_time = 0;
     int32_t hourly_last_run = 0;
+  };
+
+  class MinutelyScheduler {
+  public:
+    std::function<void (int32_t)> callback {};
+    int minutely_period = 0;
+
+    void tick(int32_t current_time);
+  private:
+    int32_t last_known_time = 0;
     int32_t minutely_last_run = 0;
   };
 }
