d90bb2b807dcceea1cd67787c20642f61027a750
kolen <incredible.angst@gmail.com>
2019-02-05 16:26:45 +0300

    Implement SensorPM measuring logic

--- src/pm_sensor/sensor_pm.cpp
+++ src/pm_sensor/sensor_pm.cpp
@@ -1,6 +1,31 @@
 #include "pm_sensor/sensor_pm.h"
 
 void pm_sensor::SensorPM::start() {
+  device.start();
 }
+
 void pm_sensor::SensorPM::tick(int32_t time) {
+  switch(state) {
+  case 0:
+    // Idle: do nothing
+    break;
+  case 1:
+    // Waiting for warmup
+    if (!measure_time) measure_time = time;
+    if (time - measure_time >= 10) {
+      state = 2;
+    }
+    break;
+  case 2:
+    device.measure();
+    device.setSleepMode(true);
+    state = 0;
+    measure_time = 0;
+    break;
+  }
+}
+
+void pm_sensor::SensorPM::measure() {
+  device.setSleepMode(false);
+  state = 1; // TODO: make enum or something like that
 }
--- test/test_sensor_pm.cpp
+++ test/test_sensor_pm.cpp
@@ -1,14 +1,41 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "pm_sensor/sensor_pm.h"
 
+using testing::InSequence;
+using testing::Return;
+using pm_sensor::PMMeasurement;
+
+class SensorPMDeviceMock : public pm_sensor::SensorPMDevice {
+public:
+  MOCK_METHOD0(start, void());
+  MOCK_METHOD1(setSleepMode, void(bool));
+  MOCK_METHOD0(measure, pm_sensor::PMMeasurement());
+};
+
 TEST(SensorPM, Init) {
+  InSequence is;
+  int32_t time_base = 1000000;
   int callback_times = 0;
-  pm_sensor::PMMeasurement measurement;
-  auto callback = [&measurement] (pm_sensor::PMMeasurement _measurement) {
+  PMMeasurement measurement;
+  auto callback = [&measurement] (PMMeasurement _measurement) {
     measurement = _measurement;
   };
-  pm_sensor::SensorPM sensor(callback);
-  ASSERT_EQ(callback_times, 0);
+  SensorPMDeviceMock device;
+  pm_sensor::SensorPM sensor(callback, device);
+  EXPECT_CALL(device, start());
+  sensor.start();
+
+  EXPECT_CALL(device, setSleepMode(false));
+  sensor.measure();
+
+  sensor.tick(time_base + 0);
+  EXPECT_CALL(device, measure()).WillOnce(Return(PMMeasurement(123, 456)));
+  EXPECT_CALL(device, setSleepMode(true));
+  sensor.tick(time_base + 9);
+  sensor.tick(time_base + 10);
+  sensor.tick(time_base + 12);
+  sensor.tick(time_base + 20);
 }
 
 int main(int argc, char **argv) {
