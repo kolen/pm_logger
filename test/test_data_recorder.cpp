4b959260709f7afaf431bb2a763d13a414a6160b
kolen <incredible.angst@gmail.com>
2019-03-03 22:38:48 +0300

    Fix data recorder shifting behavior

--- test/test_data_recorder.cpp
+++ test/test_data_recorder.cpp
@@ -14,11 +14,31 @@ TEST(DataRecorder, Basics) {
   MyDataPoint storage[6];
   pm_sensor::DataRecorder<MyDataPoint> data_recorder(storage, 6, 60);
   int32_t time_base = 23874723;
-  MyDataPoint pt = {130, 250};
-  data_recorder.add_sample(pt, time_base + 0);
-  ASSERT_EQ(pt, data_recorder.get_sample(time_base));
-  ASSERT_EQ(MyDataPoint(), data_recorder.get_sample(time_base + 60*6));
-  ASSERT_EQ(MyDataPoint(), data_recorder.get_sample(time_base + 60*100));
+  MyDataPoint pt1 = {0xa1, 0x01};
+  MyDataPoint pt2 = {0xa2, 0x02};
+  MyDataPoint pt3 = {0xa3, 0x03};
+  data_recorder.add_sample(pt1, time_base + 0);
+
+  EXPECT_EQ(pt1, data_recorder.get_at_index(0));
+  EXPECT_EQ(MyDataPoint(), data_recorder.get_at_index(1));
+  EXPECT_EQ(MyDataPoint(), data_recorder.get_at_index(2));
+
+  data_recorder.add_sample(pt2, time_base + 60);
+  data_recorder.add_sample(pt3, time_base + 60 * 2);
+
+  EXPECT_EQ(pt3, data_recorder.get_at_index(0));
+  EXPECT_EQ(pt2, data_recorder.get_at_index(1));
+  EXPECT_EQ(pt1, data_recorder.get_at_index(2));
+
+  EXPECT_EQ(pt1, data_recorder.get_at_time(time_base));
+  EXPECT_EQ(pt2, data_recorder.get_at_time(time_base + 60));
+  EXPECT_EQ(pt3, data_recorder.get_at_time(time_base + 60 * 2));
+
+  EXPECT_EQ(MyDataPoint(), data_recorder.get_at_time(time_base + 60*6));
+  EXPECT_EQ(MyDataPoint(), data_recorder.get_at_time(time_base + 60*100));
+
+  EXPECT_EQ(MyDataPoint(), data_recorder.get_at_index(6));
+  EXPECT_EQ(MyDataPoint(), data_recorder.get_at_index(20));
 }
 
 int main(int argc, char **argv) {
