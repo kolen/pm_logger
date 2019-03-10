c9d1bd3bab2e50a27974ed0ca855475baf88d472
kolen <incredible.angst@gmail.com>
2019-03-10 22:31:41 +0300

    Remove 'time' library, replace with custom code

--- src/pm_sensor/time.cpp
+++ src/pm_sensor/time.cpp
@@ -8,6 +8,8 @@
 using pm_sensor::Time;
 using pm_sensor::Logging;
 
+static const int32_t SYNC_INTERVAL = 60 * 60 * 24 * 5;
+
 #ifdef ARDUINO
 const int NTP_PACKET_SIZE = 48;
 const int NTP_LOCAL_PORT = 8123;
@@ -35,7 +37,7 @@ static void sendNTPpacket(const char* address)
   Udp.endPacket();
 }
 
-static time_t getNtpTime()
+static int32_t getNTPTime()
 {
   while (Udp.parsePacket() > 0) ; // discard any previously received packets
   Logging::println(F("Transmit NTP Request"));
@@ -57,23 +59,77 @@ static time_t getNtpTime()
   Logging::println(F("No NTP Response"));
   return 0;
 }
+
+#else
+
+static int32_t getNTPTime() {
+  // No real need to use NTP for simulation
+  return (int32_t)time(NULL);
+}
+
 #endif
 
 void Time::tick() {
+  // calling code calls now() every tick anyways, so it's currently
+  // empty
 }
 
 void Time::start() {
+  syncIfNecessary();
   #ifdef ARDUINO
   Udp.begin(NTP_LOCAL_PORT);
-  setSyncProvider(getNtpTime);
   #endif
 }
 
+#ifdef ARDUINO
+
+static inline unsigned long millis_impl() {
+  return millis();
+}
+
+#else
+
+static inline unsigned long millis_impl() {
+  // const auto base = 1551727673;
+  // return (int32_t)((time(NULL)-base)*60 + base);
+
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+
+  uint64_t millis = ts.tv_sec * 1000;
+  millis += ts.tv_nsec / 1000000;
+  return (int32_t)millis * 60;
+}
+
+#endif
+
+void Time::syncIfNecessary() {
+  if (time_since_last_sync < 0 || time_since_last_sync > SYNC_INTERVAL) {
+    Logging::println(PSTR("Syncing time"));
+    int32_t synced_time = getNTPTime();
+    if (!synced_time) { return; }
+
+    Logging::print(PSTR("Synced time: "));
+    Logging::println(synced_time);
+    last_millis = millis_impl();
+    current_time = synced_time;
+    time_since_last_sync = 0;
+  }
+}
+
 int32_t Time::now() {
-  #ifdef ARDUINO
-  return (int32_t)now();
-  #else
-  const auto base = 1551727673;
-  return (int32_t)((time(NULL)-base)*60 + base);
-  #endif
+  //printf("Current millis: %ld, time: %d, since_last_sync: %d\n", millis_impl(), current_time, time_since_last_sync);
+  syncIfNecessary();
+
+  // Time is meaningful only if time base is set
+  if (!current_time) { return 0; }
+  unsigned long current_millis = millis_impl();
+  unsigned long interval_from_last = current_millis - last_millis;
+  int32_t seconds_from_last = (int32_t)(interval_from_last / 1000);
+  current_time += seconds_from_last;
+  time_since_last_sync += seconds_from_last;
+
+  last_millis = (current_millis / 1000) * 1000;
+
+  return current_time;
 }
--- src/pm_sensor/time.h
+++ src/pm_sensor/time.h
@@ -11,8 +11,13 @@
 namespace pm_sensor {
   class Time {
   public:
-    static void start();
-    static void tick();
-    static int32_t now();
+    void start();
+    void tick();
+    int32_t now();
+  private:
+    int32_t current_time = 0;
+    int32_t time_since_last_sync = -1;
+    unsigned long last_millis = 0;
+    void syncIfNecessary();
   };
 }
