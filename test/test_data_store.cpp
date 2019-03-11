ff811839afa2f9cadb11d0c27d051ffb6c957261
kolen <incredible.angst@gmail.com>
2019-03-11 04:07:27 +0300

    Add test for PMMeasurement constructor

--- test/test_data_store.cpp
+++ test/test_data_store.cpp
@@ -1,13 +1,22 @@
 #include <gtest/gtest.h>
 #include "pm_sensor/data_store.h"
 
+using pm_sensor::float_to_stored;
+using pm_sensor::PMMeasurement;
+
 TEST(DataStore, FloatToStored) {
   uint16_t i;
   for(i=1; i<1000; i++) {
     float f = ((float)i) * 0.1;
-    ASSERT_EQ(pm_sensor::float_to_stored(f), i)
+    ASSERT_EQ(float_to_stored(f), i)
       << "Expected compact form: " << i << ", passed float: " << f;
   }
+}
+
+TEST(DataStore, ConstructPMMeasurement) {
+  PMMeasurement m(123.4, 567.8);
+  EXPECT_EQ(m.pm2_5, 1234);
+  EXPECT_EQ(m.pm10, 5678);
 }
 
 int main(int argc, char **argv) {
