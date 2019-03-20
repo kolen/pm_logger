bb2b95950dfb560c4423322fe04f6dc044c042b2
kolen <incredible.angst@gmail.com>
2019-03-21 02:51:26 +0300

    Derive various traits on characteristic structs

--- logging_node/src/characteristics.rs
+++ logging_node/src/characteristics.rs
@@ -1,5 +1,6 @@
 pub trait Characteristic {}
 
+#[derive(Debug, Clone, Copy)]
 pub struct TemperatureHumidity {
     pub temperature: i16,
     pub humidity: i16,
@@ -7,6 +8,7 @@ pub struct TemperatureHumidity {
 
 impl Characteristic for TemperatureHumidity {}
 
+#[derive(Debug, Clone, Copy)]
 pub struct PM {
     pub pm2_5: i16,
     pub pm10: i16,
