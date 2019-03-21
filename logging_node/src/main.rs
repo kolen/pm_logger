d458bfd57da660527e5c7630ca8228881545fa6b
kolen <incredible.angst@gmail.com>
2019-03-21 03:12:05 +0300

    Add code for main (temporary)

--- logging_node/src/main.rs
+++ logging_node/src/main.rs
@@ -1,3 +1,41 @@
+extern crate logging_node;
+extern crate env_logger;
+
+use logging_node::puller;
+use logging_node::characteristics::{Characteristic, TemperatureHumidity, PM};
+use std::time;
+
+fn all_samples<C>(puller: &puller::Puller) -> Vec<(time::SystemTime, C)>
+where C: Characteristic + puller::NetworkedCharacteristic {
+    // TODO: get rid of unwraps
+    let characteristic = <C as puller::NetworkedCharacteristic>::query_characteristic();
+    let boundaries = puller.get_boundaries(characteristic).unwrap();
+
+    let interval = time::Duration::from_secs(match characteristic {
+        puller::QueryCharacteristic::PM => 60 * 60,
+        puller::QueryCharacteristic::TemperatureHumidity => 60 * 10,
+    });
+
+    (0..boundaries.num_samples).scan(boundaries.last_sample_at, |time, _i| {
+        let current_time = *time;
+        let value = puller.get_recorded(current_time).unwrap();
+        *time = *time - interval;
+        Some((current_time, value))
+    }).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let puller = puller::Puller::new("pm_sensor.local:12000").unwrap();
+
+    println!(
+        "{:?} {:?}",
+        puller.get_boundaries(puller::QueryCharacteristic::PM),
+        puller.get_boundaries(puller::QueryCharacteristic::TemperatureHumidity)
+    );
+
+    println!("All PM samples:");
+    println!("{:?}", all_samples::<PM>(&puller));
+    println!("All temperature samples:");
+    println!("{:?}", all_samples::<TemperatureHumidity>(&puller));
 }
