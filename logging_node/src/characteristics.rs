35b8ee0db7dbaf710494d461f2f2518408fa11f1
kolen <incredible.angst@gmail.com>
2019-03-22 05:21:11 +0300

    Use Chrono instead of std date/time types

--- logging_node/src/characteristics.rs
+++ logging_node/src/characteristics.rs
@@ -1,3 +1,8 @@
+use crate::schema::{measurements_pm, measurements_temp_humidity};
+use chrono::{DateTime, NaiveDateTime, Utc};
+use diesel::sqlite::Sqlite;
+use diesel::Queryable;
+
 pub trait Characteristic {}
 
 #[derive(Debug, Clone, Copy)]
@@ -15,3 +20,34 @@ pub struct PM {
 }
 
 impl Characteristic for PM {}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Sample<C>
+where
+    C: Characteristic,
+{
+    pub time: DateTime<Utc>,
+    pub value: Option<C>,
+}
+
+trait CharacteristicFromColumns {
+    fn characteristic_from_columns(c1: i32, c2: i32) -> Self;
+}
+
+impl CharacteristicFromColumns for PM {
+    fn characteristic_from_columns(c1: i32, c2: i32) -> Self {
+        PM {
+            pm2_5: c1 as i16,
+            pm10: c2 as i16,
+        }
+    }
+}
+
+impl CharacteristicFromColumns for TemperatureHumidity {
+    fn characteristic_from_columns(c1: i32, c2: i32) -> Self {
+        TemperatureHumidity {
+            temperature: c1 as i16,
+            humidity: c2 as i16,
+        }
+    }
+}
