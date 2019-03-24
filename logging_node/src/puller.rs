db9f3a53a317845b2c2c248906b75ed30393da20
kolen <incredible.angst@gmail.com>
2019-03-24 05:19:10 +0300

    Implement dates retrieval for both characteristics using macros

--- logging_node/src/puller.rs
+++ logging_node/src/puller.rs
@@ -1,6 +1,6 @@
 use crate::characteristics::{Characteristic, Sample, TemperatureHumidity, PM};
 use crate::client::{Client, ClientError, NetworkedCharacteristic};
-use crate::schema::measurements_pm;
+use crate::schema::{measurements_pm, measurements_temp_humidity};
 use chrono::{DateTime, TimeZone, Utc};
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
@@ -35,9 +35,6 @@ impl From<diesel::result::Error> for PullerError {
     }
 }
 
-// Dunno how to do polymorphism on Disel level, i.e. substitute types
-// for table, for time column, etc. So lots of duplication here
-
 // Some storage-related functionality is in Characteristic module by
 // itself, so it's good idea to consolidate it into single place
 
@@ -52,22 +49,32 @@ pub trait StorableCharacteristic: Characteristic {
     ) -> Result<(), diesel::result::Error>;
 }
 
-impl StorableCharacteristic for PM {
-    fn retrieve_dates_for_range(
-        range: RangeInclusive<DateTime<Utc>>,
-        connection: &SqliteConnection,
-    ) -> Result<HashSet<DateTime<Utc>>, diesel::result::Error> {
-        Ok(measurements_pm::table
-            .select(measurements_pm::time)
-            .filter(measurements_pm::time.between(
-                range.start().timestamp() as i32,
-                range.end().timestamp() as i32,
-            ))
-            .load::<i32>(connection)?
-            .into_iter()
-            .map(|ts| Utc.timestamp(ts as i64, 0))
-            .collect())
+// Dunno how to do polymorphism on Diesel level (what are correct
+// types for tables, columns, etc), so here's C++ templates approach,
+// haha
+
+macro_rules! retrieve_dates_for_range_impl {
+    ($table:ident) => {
+        fn retrieve_dates_for_range(
+            range: RangeInclusive<DateTime<Utc>>,
+            connection: &SqliteConnection,
+        ) -> Result<HashSet<DateTime<Utc>>, diesel::result::Error> {
+            Ok($table::table
+               .select($table::time)
+               .filter($table::time.between(
+                   range.start().timestamp() as i32,
+                   range.end().timestamp() as i32,
+               ))
+               .load::<i32>(connection)?
+               .into_iter()
+               .map(|ts| Utc.timestamp(ts as i64, 0))
+               .collect())
+        }
     }
+}
+
+impl StorableCharacteristic for PM {
+    retrieve_dates_for_range_impl!(measurements_pm);
 
     fn insert_sample(
         sample: Sample<Self>,
@@ -75,6 +82,20 @@ impl StorableCharacteristic for PM {
     ) -> Result<(), diesel::result::Error> {
         insert_into(measurements_pm::table)
             .values(insert_pm_values!(&sample))
+            .execute(connection)
+            .map(|_| ())
+    }
+}
+
+impl StorableCharacteristic for TemperatureHumidity {
+    retrieve_dates_for_range_impl!(measurements_temp_humidity);
+
+    fn insert_sample(
+        sample: Sample<Self>,
+        connection: &SqliteConnection,
+    ) -> Result<(), diesel::result::Error> {
+        insert_into(measurements_temp_humidity::table)
+            .values(insert_temp_humidity_values!(&sample))
             .execute(connection)
             .map(|_| ())
     }
