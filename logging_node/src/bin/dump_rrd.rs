d30cf08bee68a673165854229d628c7ddda0d64b
kolen <incredible.angst@gmail.com>
2019-03-27 06:13:25 +0300

    Add rrd stuff

---
+++ logging_node/src/bin/dump_rrd.rs
@@ -0,0 +1,53 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate logging_node;
+
+use diesel::prelude::*;
+use dotenv::dotenv;
+use logging_node::characteristics::{Characteristic, Sample, TemperatureHumidity, PM};
+use logging_node::database;
+use logging_node::schema::measurements_temp_humidity;
+use std::process::Command;
+
+fn rrd_format_sample_number<C>(sample: Sample<C>, extract: impl Fn(C) -> f32) -> String
+where
+    C: Characteristic,
+{
+    sample
+        .value
+        .map(extract)
+        .map_or("N".into(), |v: f32| v.to_string())
+}
+
+fn main() {
+    dotenv().ok();
+    env_logger::init();
+
+    let connection = database::establish_connection();
+    let samples: Vec<Sample<TemperatureHumidity>> = measurements_temp_humidity::table
+        .order(measurements_temp_humidity::time.asc())
+        .load(&connection)
+        .expect("Can't load temp/humidity samples");
+
+    let rrd_keys = samples
+        .into_iter()
+        .map(|sample: Sample<TemperatureHumidity>| {
+            String::from(format!(
+                "{}:{}:{}",
+                sample.time.timestamp(),
+                rrd_format_sample_number(sample, |v| v.temperature()),
+                rrd_format_sample_number(sample, |v| v.humidity())
+            ))
+        });
+
+    Command::new("rrdtool").args(
+        vec![
+            String::from("update"),
+            String::from("temp_humidity.rrd"),
+            String::from("--"),
+        ]
+        .into_iter()
+        .chain(rrd_keys)
+    ).spawn().unwrap();
+}
--- logging_node/src/characteristics.rs
+++ logging_node/src/characteristics.rs
@@ -2,10 +2,21 @@ use chrono::{DateTime, Utc};
 
 pub trait Characteristic: Sized {}
 
+macro_rules! value10x {
+    ($field:ident) => {
+        pub fn $field(&self) -> f32 { self.$field as f32 * 0.1 }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct TemperatureHumidity {
     pub temperature: i16,
     pub humidity: i16,
+}
+
+impl TemperatureHumidity {
+    value10x!(temperature);
+    value10x!(humidity);
 }
 
 impl Characteristic for TemperatureHumidity {}
@@ -14,6 +25,11 @@ impl Characteristic for TemperatureHumidity {}
 pub struct PM {
     pub pm2_5: i16,
     pub pm10: i16,
+}
+
+impl PM {
+    value10x!(pm2_5);
+    value10x!(pm10);
 }
 
 impl Characteristic for PM {}
