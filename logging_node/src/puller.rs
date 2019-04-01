f021ddd0a8ec8be4f741264a05fad6b3d49f7dcf
kolen <incredible.angst@gmail.com>
2019-04-01 03:41:22 +0300

    Return result from update_characteristic

--- logging_node/src/puller.rs
+++ logging_node/src/puller.rs
@@ -32,6 +32,10 @@ impl From<diesel::result::Error> for PullerError {
     }
 }
 
+pub enum UpdateResult {
+    UpdateComplete, UpdateIncomplete,
+}
+
 impl Puller {
     pub fn new(connection: Rc<SqliteConnection>) -> Self {
         Puller {
@@ -46,7 +50,7 @@ impl Puller {
     /// Update samples of characteristic that are available on the
     /// device but isn't currently stored in the database. Polymorphic
     /// to characteristic type.
-    pub fn update_characteristic<C>(&self) -> Result<(), PullerError>
+    pub fn update_characteristic<C>(&self) -> Result<UpdateResult, PullerError>
     where
         C: Characteristic + NetworkedCharacteristic + StorableCharacteristic,
     {
@@ -57,6 +61,7 @@ impl Puller {
         let unfilled_times = self.unfilled_times::<C>()?;
         info!("Requesting new samples: {:?}", unfilled_times);
 
+        let mut complete = true;
         for time in unfilled_times {
             let result: Result<Option<C>, _> = self.client.get_recorded(time);
             match result {
@@ -69,11 +74,16 @@ impl Puller {
                 }
                 Err(e) => {
                     warn!("Error receiving sample @{}: {:?}", time, e);
+                    complete = false;
                     continue;
                 }
             }
         }
-        Ok(())
+        if complete {
+            Ok(UpdateResult::UpdateComplete)
+        } else {
+            Ok(UpdateResult::UpdateIncomplete)
+        }
     }
 
     /// Returns set of points in time which does not have samples
