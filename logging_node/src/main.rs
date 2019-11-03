10d2ac44424b22fc1e8b9d25f1f2725ad7108e65
kolen <incredible.angst@gmail.com>
2019-11-04 02:24:09 +0300

    logging_node: Add Pressure to requested characteristics

--- logging_node/src/main.rs
+++ logging_node/src/main.rs
@@ -2,10 +2,10 @@ extern crate env_logger;
 extern crate logging_node;
 
 use dotenv::dotenv;
-use logging_node::characteristics::{TemperatureHumidity, PM};
+use logging_node::characteristics::{Pressure, TemperatureHumidity, PM};
 use logging_node::database;
-use std::rc::Rc;
 use logging_node::puller::Puller;
+use std::rc::Rc;
 
 fn main() {
     dotenv().ok();
@@ -15,5 +15,8 @@ fn main() {
     let puller = Puller::new(conn);
 
     puller.update_characteristic::<PM>().unwrap();
-    puller.update_characteristic::<TemperatureHumidity>().unwrap();
+    puller
+        .update_characteristic::<TemperatureHumidity>()
+        .unwrap();
+    puller.update_characteristic::<Pressure>().unwrap();
 }
