d65b923eeeef86833cebdfb9e6fec93c285b13bb
kolen <incredible.angst@gmail.com>
2019-12-02 23:27:59 +0300

    Update homebridge plugin

--- homebridge-pm-sensor/index.js
+++ homebridge-pm-sensor/index.js
@@ -39,22 +39,24 @@ function PMSensor(log, config) {
   zis = this;
   this.socket.on("message", function(message, rinfo) {
     // FIXME: this is bug in firmware, length should be 9
-    if (message.length != 8 || message[0] != 1) {
+    if (message.length != 13 || message[0] != 1) {
       return;
     }
 
     zis.temperature = (message[1] << 8 | message[2]) * 0.1;
     zis.humidity =    (message[3] << 8 | message[4]) * 0.1;
     zis.pm2_5 =       (message[5] << 8 | message[6]) * 0.1;
-    zis.pm10 =        (message[7] << 8 | 0) * 0.1; // FIXME
+    zis.pm10 =        (message[7] << 8 | message[8]) * 0.1;
+    zis.pressure =    (message[9] << 24 | message[10] << 16 | message[11] << 8 | message[12]);
 
-    zis.log("temp: ", zis.temperature, " hum: ", zis.humidity, " pm: ", zis.pm2_5, zis.pm10);
+    zis.log("temp: ", zis.temperature, " hum: ", zis.humidity, " pm: ", zis.pm2_5, zis.pm10, "pressure: ", zis.pressure);
 
     zis.temperatureService.setCharacteristic(Characteristic.CurrentTemperature, zis.temperature);
     zis.humidityService.setCharacteristic(Characteristic.CurrentRelativeHumidity, zis.humidity);
     zis.airQualityService.setCharacteristic(Characteristic.PM2_5Density, zis.pm2_5);
     zis.airQualityService.setCharacteristic(Characteristic.PM10Density, zis.pm10);
     zis.airQualityService.setCharacteristic(Characteristic.AirQuality, zis.airQuality(zis.pm2_5, zis.pm10));
+    // There's no characteristic for pressure in HomeKit
   });
 
   setInterval(this.poll.bind(this), this.interval * 1000);
@@ -75,16 +77,37 @@ PMSensor.prototype = {
 
   airQuality: function(pm2_5, pm10) {
     // https://en.wikipedia.org/wiki/Air_quality_index#Europe
-    if (pm2_5 <= 15) {
-      return Characteristic.AirQuality.EXCELLENT;
-    } else if (pm2_5 <= 30) {
-      return Characteristic.AirQuality.GOOD;
-    } else if (pm2_5 <= 55) {
-      return Characteristic.AirQuality.FAIR;
-    } else if (pm2_5 <= 110) {
-      return Characteristic.AirQuality.INFERIOR;
-    } else {
-      return Characteristic.AirQuality.POOR;
-    }
+
+    var airQuality25 = (
+      function() {
+        if (pm2_5 <= 15) {
+          return Characteristic.AirQuality.EXCELLENT;
+        } else if (pm2_5 <= 30) {
+          return Characteristic.AirQuality.GOOD;
+        } else if (pm2_5 <= 55) {
+          return Characteristic.AirQuality.FAIR;
+        } else if (pm2_5 <= 110) {
+          return Characteristic.AirQuality.INFERIOR;
+        } else {
+          return Characteristic.AirQuality.POOR;
+        }
+      })();
+
+    var airQuality10 = (
+      function() {
+        if (pm10 <= 25) {
+          return Characteristic.AirQuality.EXCELLENT;
+        } else if (pm10 <= 50) {
+          return Characteristic.AirQuality.GOOD;
+        } else if (pm10 <= 90) {
+          return Characteristic.AirQuality.FAIR;
+        } else if (pm10 <= 180) {
+          return Characteristic.AirQuality.INFERIOR;
+        } else {
+          return Characteristic.AirQuality.POOR;
+        }
+      })();
+
+    return Math.max(airQuality25, airQuality10);
   }
 }
