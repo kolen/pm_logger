927d9e682ffb3b785538d798365454061f1df4a1
kolen <incredible.angst@gmail.com>
2019-12-20 22:40:43 +0300

    Defer computing string when doing connection

--- logging_node/src/database.rs
+++ logging_node/src/database.rs
@@ -5,5 +5,5 @@ use std::env;
 pub fn establish_connection() -> SqliteConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
