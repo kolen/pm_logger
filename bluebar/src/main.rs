2c2f74a826107b15bc289dabe7a87493a639282f
kolen <incredible.angst@gmail.com>
2019-12-23 03:55:06 +0300

    Add rtfm framework

--- bluebar/src/main.rs
+++ bluebar/src/main.rs
@@ -1,16 +1,29 @@
-#![no_std]
+#![deny(unsafe_code)]
+#![deny(warnings)]
 #![no_main]
+#![no_std]
 
-extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+use cortex_m_semihosting::{debug, hprintln};
+use panic_semihosting as _;
+use stm32f1::stm32f103;
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
+#[rtfm::app(device = stm32f1::stm32f103, peripherals = true)]
+const APP: () = {
+    #[init]
+    fn init(cx: init::Context) {
+        static mut X: u32 = 0;
 
-#[entry]
-fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+        // Cortex-M peripherals
+        let _core: cortex_m::Peripherals = cx.core;
 
-    loop {
-        // your code goes here
+        // Device specific peripherals
+        let _device: stm32f103::Peripherals = cx.device;
+
+        // Safe access to local `static mut` variable
+        let _x: &'static mut u32 = X;
+
+        hprintln!("init").unwrap();
+
+        debug::exit(debug::EXIT_SUCCESS);
     }
-}
+};
